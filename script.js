29440438754424677e090196b0e4bc379ddb3767
daniele-rosati <drosati151@gmail.com>
2024-04-09 17:24:13 +0200

    commenti

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const { createApp } = Vue;
 createApp({
-    data() {
+    data() { // creo un arrey vuoto 
         return {
             mails: [],
         };
     },
     methods: {
-        getNumberFromApi() {
+        getNumberFromApi() { // creo una funzione che mi fa una chiamata axios
+
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) =>{
-                const serverMail = response.data.response;
+                const serverMail = response.data.response; // qui mi prende le email all'interno dell'axios e le pusha all'interno dell'array vuoto
                 this.mails.push(serverMail);
 
             } )
         }
     },
     mounted() {
-        this.getNumberFromApi();
+        this.getNumberFromApi(); // qui mi fa una chiamata axios all'interno del mounted
 
     },
 
